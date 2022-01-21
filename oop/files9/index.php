996859905a915fa90d836697842d49e0797848fb
roevevgen <roevevgen7@gmail.com>
2022-01-21 19:43:42 +0200

    static

---
+++ oop/files9/index.php
@@ -0,0 +1,4 @@
+<?php
+    require_once 'modules/Math.php';
+
+    echo Math::getSum(2, 4) + Math::getProduct(3, 6);
---
+++ oop/files9/modules/Math.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    class Math
+    {
+        // Находит сумму:
+        public static function getSum($a, $b){
+            return $a + $b;
+        }
+
+        // Находит произведение:
+        public static function getProduct($a, $b)
+        {
+            return $a * $b;
+        }
+    }
