d8873d9a910abccd687af9e31c05359b239944c6
roevevgen <roevevgen7@gmail.com>
2022-01-21 23:02:38 +0200

    Num

--- oop/files9/index.php
+++ oop/files9/index.php
@@ -1,9 +1,12 @@
 <?php
     require_once 'modules/Math.php';
     require_once 'modules/Test.php';
+    require_once 'modules/Num.php';
 
     echo Math::getSum(2, 4) + Math::getProduct(3, 6) . '<br>';
     echo Math::getDoubleSum(3, 6) . '<br>';
 
 //    Test::setProperty('Hello');
-    echo Test::getProperty();
+    echo Test::getProperty() . '<br>';
+
+    echo Num::getNum1() + Num::getNum2();
---
+++ oop/files9/modules/Num.php
@@ -0,0 +1,46 @@
+<?php
+
+
+    class Num
+    {
+        private static $num1 = 2;
+        private static $num2 = 3;
+
+        public static function getSum($num1, $num2)
+        {
+            return $num1 + $num2;
+        }
+
+
+        /**
+         * @return int
+         */
+        public static function getNum1(): int
+        {
+            return self::$num1;
+        }
+
+        /**
+         * @return int
+         */
+        public static function getNum2(): int
+        {
+            return self::$num2;
+        }
+
+        /**
+         * @param int $num1
+         */
+        public static function setNum1(int $num1): void
+        {
+            self::$num1 = $num1;
+        }
+
+        /**
+         * @param int $num2
+         */
+        public static function setNum2(int $num2): void
+        {
+            self::$num2 = $num2;
+        }
+    }
