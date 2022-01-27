e8fc9e5aab904edd2ed395d42e236f8855bbe066
roevevgen <roevevgen7@gmail.com>
2022-01-27 18:24:18 +0200

    interface

--- oop/files14/index.php
+++ oop/files14/index.php
@@ -1,1 +1,7 @@
 <?php
+require_once 'modules/Disk.php';
+require_once 'modules/iCircle.php';
+require_once 'modules/iFigure.php';
+require_once 'modules/iTetragon.php';
+require_once 'modules/Rectangle.php';
+require_once 'modules/Quadrate.php';
