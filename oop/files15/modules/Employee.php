c98017674994657525b50804a1efc07368bc3676
roevevgen <roevevgen7@gmail.com>
2022-01-27 19:04:55 +0200

    interface

---
+++ oop/files15/modules/Employee.php
@@ -0,0 +1,28 @@
+<?php
+
+
+    class Employee
+    {
+        private $name;
+        private $salary;
+        public function __construct($name, $salary){
+            $this->name = $name;
+            $this->salary = $salary;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getSalary()
+        {
+            return $this->salary;
+        }
+    }
---
+++ oop/files15/modules/Programmer.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    class Programmer extends Employee implements iProgrammer
+    {
+
+        public function getLangs()
+        {
+            // TODO: Implement getLangs() method.
+        }
+
+        public function addLang($lang)
+        {
+            // TODO: Implement addLang() method.
+        }
+    }
