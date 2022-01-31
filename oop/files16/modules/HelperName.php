f0369faa5faacf3407b7708b8319c44247a645ac
roevevgen <roevevgen7@gmail.com>
2022-01-31 21:22:12 +0200

    trait HelperName

---
+++ oop/files16/modules/HelperName.php
@@ -0,0 +1,24 @@
+<?php
+
+
+    trait HelperName
+    {
+        private $name;
+        private $age;
+
+        /**
+         * @return mixed
+         */
+        public function getName()
+        {
+            return 23;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getAge()
+        {
+            return 34;
+        }
+    }
