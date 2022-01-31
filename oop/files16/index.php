465d1fa58e2838a4a00d9b185e909575fe740090
roevevgen <roevevgen7@gmail.com>
2022-01-31 21:21:54 +0200

    trait HelperName

--- oop/files16/index.php
+++ oop/files16/index.php
@@ -1,12 +1,13 @@
 <?php
 
     require_once 'modules/Helper.php';
-    require_once 'modules/User.php';
+    require_once 'modules/HelperName.php';
     require_once 'modules/City.php';
+    require_once 'modules/User.php';
 
     $user = new User('john', 30);
-    echo $user->getName() . '<br>'; // выведет 'john'
-    echo $user->getAge() . '<br>';  // выведет 30
+    echo $user->getName2() . '<br>'; // выведет 'john'
+    echo $user->getAge2() . '<br>';  // выведет 30
 
     $city = new City('Nook', 20000);
     echo $city->getName() . '<br>';
--- oop/files16/modules/User.php
+++ oop/files16/modules/User.php
@@ -3,7 +3,14 @@
 
     class User
     {
-        use Helper; // подключаем трейт
+        use Helper, HelperName {
+            HelperName::getAge insteadof Helper;
+            Helper::getAge as getAge2;
+            HelperName::getName insteadof Helper;
+            Helper::getName as getName2;
+        }
+
+        // подключаем трейт
 
         public function __construct($name, $age)
         {
