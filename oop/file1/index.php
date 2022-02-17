1ef055e85942f744a650800db9c5eb37bca4bdf1
roevevgen <roevevgen7@gmail.com>
2022-02-17 23:50:36 +0200

    composer

--- oop/file1/index.php
+++ oop/file1/index.php
@@ -1,12 +1,15 @@
 <?php
     declare(strict_types=1);
 
-    namespace ftp\file1;
 
-    use ftp\file1\modules\Car;
 
-    require __DIR__ .  '/autoloader/autoloader.php';
+    require_once __DIR__ . '/vendor/autoload.php';
 
+    use modules\Car;
+
+    $faker = Faker\Factory::create() ;
+    echo $faker->name;
+    echo $faker->address;
     $car = [
         new Car('Hummer', 'black', 45),
         new Car('Ferrari', 'red', 60),
@@ -17,4 +20,5 @@
     foreach ($car as $new) {
         echo 'Машина создана ' . $new->getName() . ' Цвет ' . $new->getColor() . ' Топливо ' . $new->getFuel() . '<br>';
     }
+    dump($car);
 
--- oop/file1/modules/Car.php
+++ oop/file1/modules/Car.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace ftp\file1\modules;
+namespace modules;
 
     class Car
     {
